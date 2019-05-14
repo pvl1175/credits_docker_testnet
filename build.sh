04696c330dc173c52f78229e2535fbe1d6c5b62f
pvl1175 <vitaly.popov.75@gmail.com>
2019-05-14 17:42:09 +0300

    update

---
+++ build.sh
@@ -0,0 +1,22 @@
+wget https://credits.com/Content/file_users/Credits_Node_linux_x64_ver_4.2.412.3_test.tar.gz -P build Credits_Node_linux_x64_ver_4.2.412.3_test.tar.gz
+tar -xvzf build/Credits_Node_linux_x64_ver_4.2.412.3_test.tar.gz --strip-components 1 -C build
+rm build/Credits_Node_linux_x64_ver_4.2.412.3_test.tar.gz
+
+g++ -pthread source/runner.cpp  -o build/runner
+cp common/* build/
+
+sudo docker build -t credits_testnet .
+sudo docker volume create test_db
+sudo docker volume create test_keys
+
+# some helpers
+#sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+# run bash
+#sudo docker run -it -p 6000:6000 -p 9090:9090 --mount source=test_db,target=/credits/test_db --mount source=test_keys,target=/credits/test_keys credits_testnet:latest bash
+
+# run node
+#sudo docker run -it -p 6000:6000 -p 9090:9090 --mount source=test_db,target=/credits/test_db --mount source=test_keys,target=/credits/test_keys credits_testnet:latest
+
+# additional example
+#./client --db-path test_db/ --public-key-file test_keys/public.txt --private-key-file test_keys/private.txt
